cb5c90c645c5f9933d53b8c0616a3be8a74097f8
Ethan Lang <ethanc.lang07@gmail.com>
2026-09-18 19:53:47 -0400

    Fix to generate hashes with asyncio

--- packer/packPack.py
+++ packer/packPack.py
@@ -1,3 +1,5 @@
+import aiohttp
+import asyncio
 import configparser
 import hashlib
 import json
@@ -7,35 +9,14 @@ import tomllib
 import urllib.request
 import zipfile
 
-os.chdir("..")
-with open("mmc-pack.json", "r") as file:
-    deps = json.load(file)
-dependencies = {}
-for entry in deps['components']:
-    match (entry['cachedName']):
-        case "Minecraft":
-            dependencies['minecraft'] = str(entry['version'])
-        case "Forge":
-            dependencies['forge'] = str(entry['version'])
-        case "NeoForge":
-            dependencies['neoforge'] = str(entry['version'])
-parser = configparser.RawConfigParser()
-parser.read("pack.cfg")
-ver = parser.get("General", "Version")
-name = parser.get("General", "Name")
-
-zip_file = zipfile.ZipFile(name + ".mrpack", 'w')
-os.chdir("minecraft")
-processed_mods = []
 files = []
 
-def parse_toml(toml_filename):
+async def parse_toml(toml_filename, zip_file):
     toml_filepath = "mods/.index/" + toml_filename
     with open(toml_filepath, "rb") as file:
         toml = tomllib.load(file)
     if toml is not None:
         if toml['download']['mode'] == 'url':
-            processed_mods.append(toml['filename'])
             vals = {}
             if (toml['side'] == "client"):
                 vals['env'] = {"client": "required", "server": "unsupported"}
@@ -43,11 +24,13 @@ def parse_toml(toml_filename):
             if (toml['side'] == "server"):
                 vals['env'] = {"client": "optional", "server": "required"}
                 print("Server only mod: " + toml['name'])
-            vals['fileSize'] = os.path.getsize("mods/" + toml['filename'])
             vals['path'] = "mods/" + toml['filename']
             download_info = toml['download']
             vals['downloads'] = [download_info['url']]
-            vals['hashes'] = calc_mod_hashes(toml['filename'])
+            fileInfo = await calc_mod_hashes(download_info['url'])
+            print("Hash calculated for " + toml['filename'])
+            vals['hashes'] = {"sha1": fileInfo['sha1'], "sha512": fileInfo['sha512']}
+            vals['fileSize'] = fileInfo['filesize']
 
             files.append(vals)
         elif toml['download']['mode'] == 'metadata:curseforge':
@@ -68,24 +51,51 @@ def parse_toml(toml_filename):
             zip_file.write(path, "overrides/mods/" + toml['filename'])
 
 
-def calc_mod_hashes(mod_filename):
-    mod_filepath = "mods/" + mod_filename
+async def calc_mod_hashes(mod_url):
     sha_1 = hashlib.sha1()
     sha_512 = hashlib.sha512()
-    with open(mod_filepath, "rb") as file:
-        while 1:
-            read_bytes = file.read(1024)
-            sha_1.update(read_bytes)
-            sha_512.update(read_bytes)
-            if not read_bytes:
-                break
+    filesize = 0
+    async with aiohttp.ClientSession() as session:
+        async with session.get(mod_url) as response:
+            while True:    
+                read_bytes = await response.content.read()
+                if not read_bytes:
+                    break
+                filesize += len(read_bytes)
+                sha_1.update(read_bytes)
+                sha_512.update(read_bytes)
+
     return {
         "sha1": sha_1.hexdigest(),
-        "sha512": sha_512.hexdigest()
+        "sha512": sha_512.hexdigest(),
+        "filesize": filesize
     }
 
-for file in os.listdir("mods/.index"):
-    parse_toml(file)
+async def get_mod_info(zip_file):
+    mod_tomls = [parse_toml(file, zip_file) for file in os.listdir("mods/.index")]
+    await asyncio.gather(*mod_tomls)
+
+os.chdir("..")
+with open("mmc-pack.json", "r") as file:
+    deps = json.load(file)
+dependencies = {}
+for entry in deps['components']:
+    match (entry['cachedName']):
+        case "Minecraft":
+            dependencies['minecraft'] = str(entry['version'])
+        case "Forge":
+            dependencies['forge'] = str(entry['version'])
+        case "NeoForge":
+            dependencies['neoforge'] = str(entry['version'])
+parser = configparser.RawConfigParser()
+parser.read("pack.cfg")
+ver = parser.get("General", "Version")
+name = parser.get("General", "Name")
+
+zip_file = zipfile.ZipFile(name + ".mrpack", 'w')
+os.chdir("minecraft")
+
+asyncio.run(get_mod_info(zip_file))
 
 for file in os.listdir("."):
     if os.path.isdir(file) and file != "mods":
